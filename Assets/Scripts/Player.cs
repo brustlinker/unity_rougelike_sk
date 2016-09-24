25f0171c2845a9d70e083ac4d6acc64681a15a97
dongshen <1634694819@qq.com>
2016-09-24 12:23:31 +0800

    完成主角移动

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+
+	private int posx = 1;
+	private int posy = 1;
+
+	private const string xAxis="Horizontal";
+	private const string yAxis="Vertical";
+
+
+	private Vector2 targetPos=new Vector2(1,1);
+	private Rigidbody2D rigidbody;
+	public float smoothing = 1;
+	public float restTime = 1;
+	public float restTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		rigidbody.MovePosition(Vector2.Lerp(transform.position,targetPos,smoothing*Time.deltaTime));
+
+
+		restTimer+=Time.deltaTime;
+		if(restTimer<restTime) return;
+
+		float h = Input.GetAxisRaw(xAxis);
+
+		//float h = 1;
+		float v = Input.GetAxisRaw(yAxis);
+		if( h > 0 )
+		{
+			v=0;
+		}
+
+
+
+		if(h!=0||v!=0)
+		{
+			targetPos += new Vector2(h,v);
+			restTimer = 0;
+		}
+	}
+}
